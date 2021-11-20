ae91805145e1644a01999e95921c26a9af84b11f
ProfessorFish <67111659+ProfessorFish@users.noreply.github.com>
2021-11-20 12:36:10 +0000

    added more options to setup

--- setup.js
+++ setup.js
@@ -1,5 +1,6 @@
 exports.public = {
     "owner": "560170131796262926",//Your discord ID
     "token": "TOKEN", //Your bot token
-    "default_prefix": "?" //The bot's default prefix
+    "default_prefix": "?", //The bot's default prefix
+    "embed_colour": "RANDOM" //The colour all embeds will be unless listed bellow
   }
