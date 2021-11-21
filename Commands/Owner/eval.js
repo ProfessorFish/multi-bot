fdb0b216af547f5fcb161e90895850ab69bf9e10
ProfessorFish <67111659+ProfessorFish@users.noreply.github.com>
2021-11-21 16:18:28 +0000

    updated eval to support mention prefix and added js styling to return value

--- Commands/Owner/eval.js
+++ Commands/Owner/eval.js
@@ -6,13 +6,17 @@ module.exports = {
   run: async function(client, message, db, config){
     if(message.author.id != config.owner)return message.channel.send("Only the bot owner can run this command!")
     const args = message.content.split(" ")
-    args.splice(0, 1)
+    if(args[0].includes(client.user.id)){
+      args.splice(0, 2)
+    } else{
+      args.splice(0, 1)
+    }
     try{
     let value = eval(args.join(" "))
     const embed = new Discord.MessageEmbed()
     .setColor(config.success_embed_colour)
     .setTitle("SUCCESS")
-    .setDescription("**RESULT:**\n" + value)
+    .setDescription("**RESULT:**\n" + "```js\n" + value + "```")
     message.channel.send({embeds: [embed]})
     } catch(err){
       const embed = new Discord.MessageEmbed()
