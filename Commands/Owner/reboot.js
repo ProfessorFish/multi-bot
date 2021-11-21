42a018d0524adb12519010a230209def9d7294d0
ProfessorFish <67111659+ProfessorFish@users.noreply.github.com>
2021-11-21 15:42:52 +0000

    added reboot command

---
+++ Commands/Owner/reboot.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js")
+require("colors")
+module.exports = {
+  triggers: ["reboot"],
+  description: "Reboots the bot. (OWNER ONLY)",
+  usage: "reboot",
+  run: async function(client, message, db, config){
+    if(message.author.id != config.owner)return message.channel.send("Only the bot owner can run this command!")
+    try{
+      var m = await message.channel.send("I'll see you on the other side :upside_down:")
+      client.destroy()
+    } catch(err){
+      message.channel.send("Could not destroy client!\nThrowing error to crash bot instead!")
+    }
+    await client.login(config.token)
+    message.channel.send("I'm back! Did you miss me?")
+    console.log(("Logged in as " + client.user.tag + " at:\n" + new Date(Date.now()).toString()).rainbow.bold.underline.bgBlack)
+  }
+}
