cced9bc9159c5ecac4771f3b15c483137ebd75d0
ProfessorFish <67111659+ProfessorFish@users.noreply.github.com>
2021-11-21 15:50:16 +0000

    fixed mention for prefix support

--- Events/message.js
+++ Events/message.js
@@ -1,7 +1,7 @@
 module.exports = {
   run: async function(client, message, db, settings){
     const args = message.content.toLowerCase().split(" ")
-    let c = client.commands.find(k=> k.triggers.find(l=>(settings.default_prefix + l == args[0]) || (message.mentions.users.first().id === client.user.id && l === args[1])))
+    let c = client.commands.find(k=> k.triggers.find(l=>(settings.default_prefix + l == args[0]) || (message.mentions.users.first() && message.mentions.users.first().id === client.user.id && l === args[1])))
     if(c){
       c.run(client, message, db, settings)
     }
