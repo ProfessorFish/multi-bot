a75e777b0b8751a87f9ef99147525895cbc81204
ProfessorFish <67111659+ProfessorFish@users.noreply.github.com>
2021-11-21 16:06:10 +0000

    updated avatar command to work with mention prefix

--- Commands/Utility/avatar.js
+++ Commands/Utility/avatar.js
@@ -1,3 +1,4 @@
+require("colors")
 const Discord = require("discord.js")
 module.exports = {
   triggers: ["avatar", "av"],
@@ -6,6 +7,7 @@ module.exports = {
   run: async function(client, message, db, config){
     const args = message.content.split(" ")
     let user = await resolveUser(message)
+    console.log(user)
     if(!user)return message.channel.send("No way to identify user!")
     var img = user.displayAvatarURL({dynamic: true})
 
@@ -14,18 +16,25 @@ module.exports = {
     .setImage(img)
     .setAuthor(user.tag, img, img)
     .setThumbnail(img)
-    .setFooter(user.tag, img)
+    .setFooter(user.tag, img, img)
     message.channel.send({embeds: [embed]})
 
     async function resolveUser(message){
-      if(message.mentions.users.first()){
+      if(message.mentions.users.first() && !args[0].includes(client.user.id)){
         return message.mentions.users.first()
-      } else if(args[1]){
+      }else if(message.mentions.users.first() && (args[0].includes(client.user.id) && message.mentions.users.toJSON()[1])){
+      return message.mentions.users.toJSON()[1]
+      } else if(!args[0].includes(client.user.id) && args[1]){
         if(!args[1] || !parseInt(args[1]))return false
         let a = await message.guild.members.fetch(args[1])
         if(!a)return false
         return a.user
-      } else{
+      } else if(args[0].includes(client.user.id) && args[2]){
+        if(!args[2] || !parseInt(args[2]))return false
+        let a = await message.guild.members.fetch(args[2])
+        if(!a)return false
+        return a.user
+      }else{
         return message.author
       }
     }
