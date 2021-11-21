f166df055c0965fcdf019ec6be36913b1de1ec91
ProfessorFish <67111659+ProfessorFish@users.noreply.github.com>
2021-11-21 19:39:39 +0000

    added uptime command and pretty print for times

--- Commands/Info/info.js
+++ Commands/Info/info.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js")
+const pretty = require("pretty-ms")
 module.exports = {
   triggers: ["info", "info"],
   description: "Display information about the bot.",
@@ -8,14 +9,14 @@ module.exports = {
     const embed = new Discord.MessageEmbed()
     .setColor(config.embed_colour)
     .setThumbnail(client.user.displayAvatarURL({dynamic: true}))
-    .setDescription(`OS: ${os}\nCommand count: ${client.commands.length}\nBot creation date: ${Date(client.user.createdTimestamp).toString()}\nOnline since: ${Date(client.uptime)} (${client.uptime/60000}mins ago)\nPing: Pinging`)
+    .setDescription(`OS: ${os}\nCommand count: ${client.commands.length}\nBot creation date: ${Date(client.user.createdTimestamp).toString()}\nOnline since: ${Date(client.uptime)} (${pretty(client.uptime)} ago)\nPing: Pinging`)
     .setAuthor(client.user.tag, client.user.displayAvatarURL({dynamic: true}), "https://github.com/ProfessorFish/multi-bot")//Remove the link and the comma after author if you want to :)
     .setFooter("Forked off of: https://github.com/ProfessorFish/multi-bot")//Feel free to remove the credit :)
     var m = await message.channel.send({embeds: [embed]})
     const embed2 = new Discord.MessageEmbed()
     .setColor(config.embed_colour)
     .setThumbnail(client.user.displayAvatarURL({dynamic: true}))
-    .setDescription(`OS: ${os}\nCommand count: ${client.commands.length}\nBot creation date: ${Date(client.user.createdTimestamp).toString()}\nOnline since: ${Date(client.uptime)} (${client.uptime/60000}mins ago)\nPing: ${m.createdTimestamp - message.createdTimestamp}ms`)
+    .setDescription(`OS: ${os}\nCommand count: ${client.commands.length}\nBot creation date: ${Date(client.user.createdTimestamp).toString()}\nOnline since: ${Date(client.uptime)} (${pretty(client.uptime)} ago)\nPing: ${m.createdTimestamp - message.createdTimestamp}ms`)
     .setAuthor(client.user.tag, client.user.displayAvatarURL({dynamic: true}), "https://github.com/ProfessorFish/multi-bot")//Remove the link and the comma after author if you want to :)
     .setFooter("Forked off of: https://github.com/ProfessorFish/multi-bot")//Feel free to remove the credit :)
     m.edit({embeds: [embed2]})
---
+++ Commands/Utility/uptime.js
@@ -0,0 +1,9 @@
+const pretty = require("pretty-ms")
+module.exports = {
+  triggers: ["uptime"],
+  description: "Display the uptime of the bot.",
+  usage: "uptime",
+  run: async function(client, message, db){
+    message.channel.send(`I have been online since:\n**${Date(Date.now()-client.uptime)}**\nWhich is:\n**${pretty(client.uptime)} ago**`)
+  }
+}
