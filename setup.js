bb916580fc8132ce4cc41cced0352488a6263a45
ProfessorFish <67111659+ProfessorFish@users.noreply.github.com>
2021-11-21 12:57:03 +0000

    test part 2

--- setup.js
+++ setup.js
@@ -1,6 +1,6 @@
 exports.public = {
   "owner": "560170131796262926",//Your discord ID
-  "token": process.env.TOKEN, //Your bot token
+  "token": process.env.TOKEN, //Your bot's token
   "default_prefix": "?", //The bot's default prefix
   "embed_colour": "RANDOM", //Default embed colour
   "success_embed_colour": "GREEN", //Embed colour on success
