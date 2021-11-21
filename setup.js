f9e4252a971a62ee393231b4a707396cb9d5672a
ProfessorFish <67111659+ProfessorFish@users.noreply.github.com>
2021-11-21 12:52:48 +0000

    Added more to setup

--- setup.js
+++ setup.js
@@ -1,6 +1,8 @@
 exports.public = {
-    "owner": "560170131796262926",//Your discord ID
-    "token": "TOKEN", //Your bot token
-    "default_prefix": "?", //The bot's default prefix
-    "embed_colour": "RANDOM" //The colour all embeds will be unless listed bellow
-  }
+  "owner": "560170131796262926",//Your discord ID
+  "token": process.env.TOKEN, //Your bot token
+  "default_prefix": "?", //The bot's default prefix,
+  "embed_colour": "RANDOM", //Default embed colour
+  "success_embed_colour": "GREEN", //Embed colour on success
+  "fail_embed_colour": "RED" //Embed colour on fail
+}
