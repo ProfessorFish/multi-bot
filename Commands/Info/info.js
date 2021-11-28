efc4bb1d6bf615db5be41548ba6232316ebe37a7
ProfessorFish <67111659+ProfessorFish@users.noreply.github.com>
2021-11-28 12:47:04 +0000

    added guild count to info embed

--- Commands/Info/info.js
+++ Commands/Info/info.js
@@ -15,7 +15,7 @@ module.exports = {
     const embed2 = new Discord.MessageEmbed()
     .setColor(config.embed_colour)
     .setThumbnail(client.user.displayAvatarURL({dynamic: true}))
-    .setDescription(`OS: ${os}\nCPU usage: ${cpu.user/cpu.system}%\nMemory usage: ${memory.heapTotal/memory.heapUsed}%\nCommand count: ${client.commands.length}\nBot creation date: ${new Date(client.user.createdTimestamp).toString()} **(${pretty(Date.now() - client.user.createdTimestamp)} ago)**\nOnline since: ${new Date(client.uptime)} **(${pretty(client.uptime)} ago)**\nPing: ${m.createdTimestamp - message.createdTimestamp}ms`)
+    .setDescription(`OS: ${os}\nCPU usage: ${cpu.user/cpu.system}%\nMemory usage: ${memory.heapTotal/memory.heapUsed}%\nGuild Count: ${client.guilds.cache.size}\nCommand count: ${client.commands.length}\nBot creation date: ${new Date(client.user.createdTimestamp).toString()} **(${pretty(Date.now() - client.user.createdTimestamp)} ago)**\nOnline since: ${new Date(client.uptime)} **(${pretty(client.uptime)} ago)**\nPing: ${m.createdTimestamp - message.createdTimestamp}ms`)
     .setAuthor(client.user.tag, client.user.displayAvatarURL({dynamic: true}), "https://github.com/ProfessorFish/multi-bot")//Remove the link and the comma after author if you want to :)
     .setFooter("Forked off of: https://github.com/ProfessorFish/multi-bot")//Feel free to remove the credit :)
     m.edit({embeds: [embed2]})
