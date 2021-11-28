82cfc8c2e71ca9f0369ca76eaafc640a9c45ea98
ProfessorFish <67111659+ProfessorFish@users.noreply.github.com>
2021-11-28 17:36:01 +0000

    fixed avatar command for new args system

--- Commands/Utility/avatar.js
+++ Commands/Utility/avatar.js
@@ -3,8 +3,7 @@ module.exports = {
   triggers: ["avatar", "av"],
   description: "Display the ping of the bot to Discord.",
   usage: "avatar [@user || userID]",
-  run: async function(client, message, db, config){
-    const args = message.content.split(" ")
+  run: async function(client, message, db, config, args){
     let user = await resolveUser(message)
     if(!user)return message.channel.send("No way to identify user!")
     var img = user.displayAvatarURL({dynamic: true})
@@ -14,7 +13,7 @@ module.exports = {
     .setImage(img)
     .setAuthor(user.tag, img, img)
     .setThumbnail(img)
-    .setFooter(user.tag, img, img)
+    .setFooter(user.tag, img)
     message.channel.send({embeds: [embed]})
 
     async function resolveUser(message){
@@ -22,14 +21,9 @@ module.exports = {
         return message.mentions.users.first()
       }else if(message.mentions.users.first() && (args[0].includes(client.user.id) && message.mentions.users.toJSON()[1])){
       return message.mentions.users.toJSON()[1]
-      } else if(!args[0].includes(client.user.id) && args[1]){
+      } else if(args[1]){
         if(!args[1] || !parseInt(args[1]))return false
         let a = await message.guild.members.fetch(args[1])
-        if(!a)return false
-        return a.user
-      } else if(args[0].includes(client.user.id) && args[2]){
-        if(!args[2] || !parseInt(args[2]))return false
-        let a = await message.guild.members.fetch(args[2])
         if(!a)return false
         return a.user
       }else{
